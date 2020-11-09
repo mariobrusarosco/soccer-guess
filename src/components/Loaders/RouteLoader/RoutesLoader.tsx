877c7f84f3f95e5c2751b76ad98e7739bd8ea79e
Mario Brusarosco <mariobrusarosc@hotmail.com>
2020-11-09 00:32:37 -0300

    Fixing the loaer

--- src/components/Loaders/RouteLoader/RoutesLoader.tsx
+++ src/components/Loaders/RouteLoader/RoutesLoader.tsx
@@ -3,16 +3,11 @@ import styled from "styled-components";
 import Pulse from "../Pulse";
 
 const Container = styled.div`
-  .body {
-    height: calc(100vh - #{$header-height});
-    height: calc(100vh - var(--header-height));
-    width: 100%;
-    background-color: $primary-color;
-    background-color: var(---primary-color);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 const Wrapper = styled.div`
   width: 50px;
